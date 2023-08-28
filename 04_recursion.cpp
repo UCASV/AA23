0ccd99ab8bf3693937dd42f9e5d4a13545f3938a
enxel <earaujo@uca.edu.sv>
2023-08-28 15:03:45 -0600

    Fibonacci example added.

--- 04_recursion.cpp
+++ 04_recursion.cpp
@@ -11,7 +11,7 @@ using namespace std;
 //Recursión por Posposición
 int factorial(int n){
     if(n == 0) return 1;        //Condición de paro
-    return n * factorial(n-1);  //Llamada recursiva con trabajo pendiente
+    return n * factorial(n - 1);  //Llamada recursiva con trabajo pendiente
 }
 /*
     4*____      4*6=24
@@ -23,8 +23,8 @@ int factorial(int n){
 
 //Recursión por Cola
 int factorial_cola(int acc, int cnt, int n){ //Se usa un acumulador y un contador
-    if(cnt == n) return acc*cnt;               //Condición de paro
-    return factorial_cola(acc*cnt, cnt+1, n);  //Llamada recursiva sin trabajo pendiente
+    if(cnt == n) return acc * cnt;               //Condición de paro
+    return factorial_cola(acc * cnt, cnt + 1, n);  //Llamada recursiva sin trabajo pendiente
 }
 /*
     acc     cont    n
@@ -35,9 +35,34 @@ int factorial_cola(int acc, int cnt, int n){ //Se usa un acumulador y un contado
      24
 */
 
+//Fibonacci
+int fibonacci(int n){
+    if(n == 0 || n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+/*
+    La ejecución de la función fibonacci, y la ejecución de toda función recursiva
+    de manera natural provoca una estructura de árbol entre las distintas instancias
+    en memoeria durante el proceso:
+
+                                   fibonacci(3) ...
+                                  /
+                      fibonacci(4)
+                     /            \
+                    /              fibonacci(2) ...
+        fibonacci(5)
+                    \              fibonacci(2) ...
+                     \            /
+                      fibonacci(3)
+                                  \
+                                   fibonacci(1) -> 1
+*/
+
 int main() {
     cout << "El factorial de 4 es: " << factorial(4) << "\n\n";
-    cout << "El factorial de 4 es: " << factorial_cola(1,1,4) << "\n";
+    cout << "El factorial de 4 es: " << factorial_cola(1,1,4) << "\n\n";
+
+    cout << "El quinto término de la sucesión de Fibonacci es: " << fibonacci(5) << "\n";
 
     return 0;
 }
